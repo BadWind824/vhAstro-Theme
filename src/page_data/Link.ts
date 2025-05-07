7217b5f939efaf3047912c3f4086daa1b98ac8a7
BadWind824 <badwind824@163.com>
2025-05-07 11:35:49 +0800

    更新 Link.ts

--- src/page_data/Link.ts
+++ src/page_data/Link.ts
@@ -3,12 +3,7 @@ export default {
   api: '',
   // api 为空则使用 data 静态数据
   data: [
-    {
-      "name": "时雨の主页",
-      "link": "https://1105.badwind824.top/",
-      "avatar": "https://q1.qlogo.cn/g?b=qq&nk=1305740702&s=640",
-      "descr": "袖丈が覚束ない夏の終わり."
-    },
+    
    
   ]
 }
