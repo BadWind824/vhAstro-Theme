0f22db000cab7dd8a0361a794a8a20944dfd7c59
BadWind824 <badwind824@163.com>
2025-05-07 12:58:01 +0800

    更新 Talking.ts

--- src/page_data/Talking.ts
+++ src/page_data/Talking.ts
@@ -9,7 +9,8 @@ export default {
       "tags": [
         "LivePhoto",
       ],
-      "content": "🥰 ::vhLivePhoto{photo=\"https://i.uik.cc/view.php/8f8e0231a9349b19bdcfe1f25fa33ada.png\" video=\"https://i.uik.cc/view.php/ec578961193075680f9d2479d38a27b6.mp4\" type=\"y\"}::vhLivePhoto{photo=\"https://i.uik.cc/view.php/b5efd5ccbd0794b4d80ea314038e570e.png\" video=\"https://i.uik.cc/view.php/2d7aceacc1e5c4196f82c8b672bb2f85.mp4\" type=\"y\"}"
+      "content": "🥰::vhLivePhoto{photo="https://astro.badwind824.top/assets/livephoto/1.webp" video="https://astro.badwind824.top/assets/livephoto/1.mp4" type="y"}
+::vhLivePhoto{photo="https://astro.badwind824.top/assets/livephoto/2.webp" video="https://astro.badwind824.top/assets/livephoto/2.mp4" type="y"} "
     },
     {
       "date": "2025-05-07 11:30:16",
